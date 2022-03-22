9ce382762b1eaa94911b8abb9cc4ab49494bbcf2
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2022-03-22 16:18:39 +0200

    ADD: custom lebal

--- components/use-countries/HookCountries.ts
+++ components/use-countries/HookCountries.ts
@@ -1,16 +1,22 @@
-import styled from 'styled-components';
-import { HookCountriesRaw } from './HookCountriesRaw';
+import styled from "styled-components";
+import { HookCountriesRaw } from "./HookCountriesRaw";
 
 export const HookCountries = styled(HookCountriesRaw)`
   display: grid;
-  grid-auto-flow:column;
+  grid-auto-flow: column;
   grid-column-gap: 2em;
   margin: 2em;
 
-  .geo{
-    font-size:0.5cm;
+  .geo {
+    font-size: 0.5cm;
     padding: 0.5em;
-    min-width:20em;
+    min-width: 25em;
+  }
+
+  .country {
+    display: grid;
+    grid-auto-flow: column;
+    grid-column-gap: 0.5em;
+    justify-content:start;
   }
 `;
-
--- components/use-countries/HookCountriesRaw.tsx
+++ components/use-countries/HookCountriesRaw.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { ChangeEventHandler, useState } from "react";
 import useCountries from "use-countries";
-import Select, { ActionMeta, OnChangeValue } from 'react-select';
+import Select, { ActionMeta, GetOptionLabel, OnChangeValue } from 'react-select';
 import makeAnimated from 'react-select/animated';
 import { Country } from "use-countries/dist/locales";
 
@@ -34,6 +34,15 @@ export const HookCountriesRaw = ({ className }: IWithClass) => {
 
   const selected = countries.find(c => c.code === country);
 
+  const getLabel = (country: Country | undefined) => {
+    if(country == null) return '';
+    const text = country.name === country.native ? country.name : `${country.name} / ${country.native}`
+    return <div className="country">
+      <span className='icon'>{country.phone}</span>
+      <span className='text'>{text}</span>
+    </div>
+  }
+
   return (
     <div className={className}>
        <Select<Country>
@@ -42,7 +51,7 @@ export const HookCountriesRaw = ({ className }: IWithClass) => {
           defaultValue={selected}
           // defaultInputValue={country}
           
-          getOptionLabel={o => o.name === o.native ? o.name : `${o.name} / ${o.native}`}
+          getOptionLabel={getLabel as ((country: Country | undefined) => string)}
           getOptionValue= {o => o.code}
           isDisabled={!countries?.length}
           isLoading={!countries?.length}
